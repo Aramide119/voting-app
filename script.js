59f0a638e6dc69059e704dcab0f1eecdc115fb2a
olawale kudirat <98582703+Aramide119@users.noreply.github.com>
2024-03-05 13:59:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function closeModal() {
+    // Close the modal
+    document.getElementById('exampleModalCenter').style.display = 'none';
+}
+
+// Automatically show the modal when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('exampleModalCenter').style.display = 'block';
+});
