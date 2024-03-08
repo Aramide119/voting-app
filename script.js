4ce78093ad7509d4aeabb5ab852385ef7fbc9073
Olawale Kudirat <folawale119@gmail.com>
2024-03-08 08:42:44 +0100

    changed voting page

--- script.js
+++
@@ -1,9 +0,0 @@
-function closeModal() {
-    // Close the modal
-    document.getElementById('exampleModalCenter').style.display = 'none';
-}
-
-// Automatically show the modal when the page loads
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('exampleModalCenter').style.display = 'block';
-});
